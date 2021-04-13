930055bd7be28420c86bcb007efdd6f5e00ca9f8
Joao Paulo <jp.contatoprofissional@gmail.com>
2021-04-13 18:32:13 -0300

    mascara valor produto

--- index.js
+++ index.js
@@ -10,6 +10,42 @@ function clearErrors(){
     })
 }
 
+function applyMask(evt) {
+    evt.preventDefault()
+    if(["0","1","2","3","4","5","6","7","8","9"].indexOf(evt.key) == -1) {
+        // console.log("letra")
+    } else { 
+        // console.log("1",evt.target.value)
+        let value = evt.target.value.replace(/^0,/, "").replace(",","").replace(/\./g, "") + evt.key
+        if(value.length <= 2) {
+            evt.target.value = "0," +value
+            // console.log("2",evt.target.value, value.length, value)
+        } else {
+            evt.target.value = value.slice(0,-2) + ',' + value.slice(value.length-2,value.length) 
+            // console.log("3",evt.target.value, value.length, value, value.slice(0,value.length-2), value.slice(value.length-2,value.length))
+        }
+        lastIndex = -1
+        value = evt.target.value.replace(/^0,[0-9]+/, "").replace(/,[0-9]+$/,"").replace(/\./g, "")
+        // console.log(value)
+        if (value.length >= 4) {
+            valuefinal = [];
+            for (let i = value.length; i>=0; i--) {
+                if ((value.length-i)%3 == 0 && value.slice(i-3, i)) {
+                    valuefinal.push(value.slice(i-3, i))
+                    lastIndex=i
+                }
+            }
+            // console.log(valuefinal)
+            valufinalstring = valuefinal.reverse().join(".")
+            evt.target.value = valufinalstring + "," + evt.target.value.replace(/^[0-9.]+,/, "")
+            if (value.slice(0,lastIndex-3)) {
+                evt.target.value = value.slice(0,lastIndex-3) + '.' + evt.target.value
+            }
+            
+        }
+    }
+}
+
 reescreveLista()
 
 document.getElementById('nameTransaction').addEventListener('keyup', (ev) => {
